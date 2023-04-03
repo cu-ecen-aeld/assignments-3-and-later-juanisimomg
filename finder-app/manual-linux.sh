7ea40e6c5642194971743020944b387c84d06ab9
juanisimomg <jugo6138@colorado.edu>
2023-04-02 18:01:40 -0700

    Updated manual linux file

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,6 +13,8 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
+SYSROOT=$(aarch64-none-linux-gnu-gcc -print-sysroot)
+
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -35,9 +37,25 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    # Deep clean.
+    echo "Deep clean step"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+    # Defconfig.
+    echo "Defconfig step"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    # Build kernel image for booting with QEMU.
+    echo "All step"
+    make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+    # Build any kernel modules.
+    # echo "Modules step"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+    # Build device tree.
+    echo "DTBS step"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +66,14 @@ then
 fi
 
 # TODO: Create necessary base directories
+# Create directory for the root filesystem.
+mkdir -p "${OUTDIR}/rootfs"
+# Go to the root of the filesystem to allow relative paths.
+cd "${OUTDIR}/rootfs"
+# Create relative path folders.
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +81,50 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # TODO: Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+
+
 
 # TODO: Make device nodes
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer ${OUTDIR}/rootfs/home
+cp -r ../conf writer.sh finder.sh finder-test.sh autorun-qemu.sh "${OUTDIR}/rootfs/home/"
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs/
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs/
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
